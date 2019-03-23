69ecdd412a006ab068c40eecffe12ba35caf5c60
마명재 <yuemj@naver.com>
2019-03-23 15:23:04 +0900

    페이지방식에서 모듈방식으로 카피

--- src/App.js
+++ src/App.js
@@ -1,28 +1,105 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const Header = (props) => {
+  console.log(props);
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: {props.totalPlayers}</span>
+    </header>
+  );
+}
+
+class Counter extends React.Component {
+  // state = {
+  //  score: 30
+  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "a",
+      score: 30
+    }
+    //this.incrementScore = this.incrementScore.bind(this);
+  }
+
+  decrementScore = () => {
+    this.setState(prevState => {
+      return {
+        score: prevState.score - 1
+      }
+    });
+  }
+  incrementScore = () => {
+    // this.state.score = this.state.score + 1; 안됨
+    // 변경을 하기 위해서는 setState를 호출
+    this.setState(prevState => {
+      return {
+        score: prevState.score + 1
+      }
+    });
+    //this.setState({score:this.state.score + 1});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="counter">
+        <button className="counter-action decrement" onClick={this.decrementScore}> -</button>
+        <span className="counter-score">{this.state.score}</span>
+        <button className="counter-action increment" onClick={this.incrementScore}> +</button>
+      </div>
+    );
+    /*<button className="counter-action increment" onClick={this.incrementScore()}> +</button> // 잘못된예 */
+  }
+}
+
+const Player = (props) => (
+  <div className="player">
+      <span className="player-name">
+        <button className="remove-player" onClick={()=>props.handleRemovePlayer(props.id)}>X</button>
+      </span>
+    <span className="player-name">{props.name}</span>
+    <Counter/>
+  </div>
+);
+
+class App extends React.Component {
+  state = {
+    players: [
+      {name: 'LDK', score: 30, id: 1},
+      {name: 'HONG', score: 40, id: 2},
+      {name: 'KIM', score: 50, id: 3},
+      {name: 'PARK', score: 60, id: 4},
+    ]
+  }
+
+  handleRemovePlayer = (id) => {
+    console.log(id);
+    // 해당 id를 삭제
+    this.setState
+    (
+      prevState => ({
+        players: prevState.players.filter(item => item.id !== id)
+      })
+    )
+  }
+
+  render() {
+    return (
+      <div className="scoreboard">
+        <Header title="My scoreboard" totalPlayers={1 + 10}/>
+        {
+          this.state.players.map(player => (
+            // score={player.score}
+            <Player name={player.name} key={player.id.toString()}
+                    id={player.id}    handleRemovePlayer={this.handleRemovePlayer}/>
+          ))
+        }
       </div>
     );
   }
 }
+
 
 export default App;
